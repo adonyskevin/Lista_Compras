2e3e8e57fe4572a63c160fcb7eeddd5ff0c9e64d
Adonys Kevin <adonyskevinms@gmail.com>
2021-07-31 23:15:25 -0300

    Adicionando validação de quantidade e valor do produto e inserindo produto na lista

--- app/src/main/java/com/adonyskevin/listadecompras/CadastroActivity.kt
+++ app/src/main/java/com/adonyskevin/listadecompras/CadastroActivity.kt
@@ -11,16 +11,30 @@ class CadastroActivity : AppCompatActivity() {
         setContentView(R.layout.activity_cadastro)
 
         val edt_produto = findViewById<EditText>(R.id.edt_produto)
+        val edt_qtd_produto = findViewById<EditText>(R.id.edt_qtd_produto)
+        val edt_valor = findViewById<EditText>(R.id.edt_valor)
         val btn_inserir = findViewById<Button>(R.id.btn_inserir)
 
         btn_inserir.setOnClickListener {
+            //Recebendo os valores digitados pelo usuário
             val produto = edt_produto.text.toString()
-            if (produto.isNotEmpty()){
-                //Enviar o item pra lista
-                //adapter.add(produto)
+            val qtd = edt_qtd_produto.text.toString()
+            val valor = edt_valor.text.toString()
+
+            if ((produto.isNotEmpty()) && (qtd.isNotEmpty()) && (valor.isNotEmpty())){
+                //Enviando o item para a lista
+                val prod = Produto(produto, qtd.toInt(), valor.toDouble())
+                
+                produtosGlobal.add(prod)
+
                 edt_produto.text.clear()
-            }else
-                edt_produto.error = "Informe um produto"
+                edt_qtd_produto.text.clear()
+                edt_valor.text.clear()
+            }else {
+                edt_produto.error = if(edt_produto.text.isEmpty()) "Informe um produto" else null
+                edt_qtd_produto.error = if(edt_qtd_produto.text.isEmpty()) "Informe a quantidade" else null
+                edt_valor.error = if(edt_valor.text.isEmpty()) "Informe o valor" else null
+            }
         }
     }
 }
---
+++ app/src/main/java/com/adonyskevin/listadecompras/Utils.kt
@@ -0,0 +1,4 @@
+package com.adonyskevin.listadecompras
+
+//Variável que armazenará os produtos da lista
+val produtosGlobal = mutableListOf<Produto>()
